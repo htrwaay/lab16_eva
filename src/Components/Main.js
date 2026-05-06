b9324a27c81fc4e0df15b654e2af293dcaba3e8a
Анастасия <xarychkina@gmail.com>
2026-05-06 19:22:12 +0300

    Задание 10-11

---
+++ src/Components/Main.js
@@ -0,0 +1,8 @@
+export function Main() {
+    return (
+        <div className="Main">
+            <h1>Welcome to the Main Page</h1>
+            <p>This is the main content of the application.</p>
+        </div>
+    );
+}
